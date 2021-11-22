edd635177ef79c7ddd0d0c885fb39b409d011018
Arai-kun <koki.alright@gmail.com>
2021-11-22 16:59:09 +0900

    c

--- client/src/app/dummy/dummy.component.ts
+++ client/src/app/dummy/dummy.component.ts
@@ -10,8 +10,8 @@ export class DummyComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    window.location.href = 
-    "https://sellercentral.amazon.co.jp/apps/authorize/consent?application_id=amzn1.sp.solution.64bd6392-c1a0-4951-9b00-2744796fc74a&version=beta";
+    //window.location.href = 
+    //"https://sellercentral.amazon.co.jp/apps/authorize/consent?application_id=amzn1.sp.solution.64bd6392-c1a0-4951-9b00-2744796fc74a&version=beta";
   }
 
 }
