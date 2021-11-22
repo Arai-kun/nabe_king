58f463750dc647d2fe37f1084cc8d38c8304c8cb
Arai-kun <koki.alright@gmail.com>
2021-11-22 21:55:58 +0900

    c

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -29,7 +29,7 @@ authRouter.get('/logout', function(req, res, next){
 });
 
 authRouter.post('/exchange', function(req, res, next){
-    console.log(req.body);
+    console.log(req);
     getTokenFromCode(req.body)
     .then(data => res.json(data))
     .catch(error => {
