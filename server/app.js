69ced2793259161c96f009584d5e3349798959a5
Arai-kun <koki.alright@gmail.com>
2021-11-22 12:57:28 +0900

    commit

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/dist/client')));
+app.use('/*', express.static(path.join(__dirname, '../client/dist/client/index.html')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
