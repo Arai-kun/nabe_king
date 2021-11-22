59434505c066768df00e0695bfe26c2702286c3a
Arai-kun <koki.alright@gmail.com>
2021-11-22 13:30:47 +0900

    c

--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -10,7 +10,7 @@ export class AuthComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    window.location.href = "https://sellercentral.amazon.com/apps/authorize/consent?application_id=amzn1.sp.solution.64bd6392-c1a0-4951-9b00-2744796fc74a&version=beta";
+    window.location.href = "https://sellercentral.amazon.com.jp/apps/authorize/consent?application_id=amzn1.sp.solution.64bd6392-c1a0-4951-9b00-2744796fc74a&version=beta";
   }
 
 }
