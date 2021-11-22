1465d620310b2429099d1fc8065620f75d0c825f
Arai-kun <koki.alright@gmail.com>
2021-11-22 13:08:20 +0900

    commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthComponent } from './auth/auth.component';
+import { NaviComponent } from './navi/navi.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "auth", pathMatch: "full" },
-  { path: "auth", component: AuthComponent }
+  { path: "auth", component: AuthComponent },
+  { path: "navi", component: NaviComponent }
 ];
 
 @NgModule({
