8509d50d919522dd31feee93b3d031ef6749aab9
Arai-kun <koki.alright@gmail.com>
2021-11-22 21:10:07 +0900

    c

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
+import { HttpClient } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +30,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { shareReplay, tap, catchError} from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
