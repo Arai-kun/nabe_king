0db57295f835d97ed44dbff9a9d56597644e942d
Arai-kun <koki.alright@gmail.com>
2021-11-22 16:45:20 +0900

    c

--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -24,7 +24,7 @@ export class AuthComponent implements OnInit {
     else
     {
       console.log(false);
-      this.url = `${this.params["amazon_callback_uri"]}?redirect_uri=https://enginestarter.nl/#/authorize&amazon_state=${this.params["amazon_state"]}&version=beta`;
+      window.location.href = `${this.params["amazon_callback_uri"]}?redirect_uri=https://enginestarter.nl/#/authorize&amazon_state=${this.params["amazon_state"]}&version=beta`;
       //window.location.href = 
       //"https://sellercentral.amazon.co.jp/apps/authorize/consent?application_id=amzn1.sp.solution.64bd6392-c1a0-4951-9b00-2744796fc74a&version=beta";
     }
