9341b2653df6ce9f8a52674a6aa7680680aabd8f
Arai-kun <koki.alright@gmail.com>
2021-11-22 16:30:19 +0900

    c

--- client/src/app/dummy/dummy.component.ts
+++ client/src/app/dummy/dummy.component.ts
@@ -11,7 +11,7 @@ export class DummyComponent implements OnInit {
 
   ngOnInit(): void {
     window.location.href = 
-    "https://sellercentral.amazon.com/apps/authorize/consent?application_id=amzn1.sp.solution.64bd6392-c1a0-4951-9b00-2744796fc74a&version=beta";
+    "https://sellercentral.amazon.co.jp/apps/authorize/consent?application_id=amzn1.sp.solution.64bd6392-c1a0-4951-9b00-2744796fc74a&version=beta";
   }
 
 }
