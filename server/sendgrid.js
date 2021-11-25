eef56a28982765a18c344ef9ea01867b1932c31e
Arai-kun <koki.alright@gmail.com>
2021-11-25 11:08:44 +0900

    c

---
+++ server/sendgrid.js
@@ -0,0 +1,12 @@
+const sendgrid = require('@sendgrid/mail');
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY || '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');
+
+async function sendgridExample() {
+  await sendgrid.send({
+    to: 'koki.alright@gmail.com',
+    from: 'noreply@mail.nabeking.com',
+    subject: 'test',
+    html: '<h1>abc123</h1>'
+  });
+}
+sendgridExample();
