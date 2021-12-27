f3c0f05f67483380ad8cfeddc6701d2c1fe223e2
Arai-kun <koki.alright@gmail.com>
2021-12-27 18:09:14 +0900

    c

--- server/scheduler.js
+++ server/scheduler.js
@@ -117,6 +117,7 @@ async function dataUpdate(user, config) {
         });
         const limit = result.headers['x-amzn-ratelimit-limit'];
         result = JSON.parse(result.body).payload;
+        console.log(result);
         for(let order of result.Orders){
             orderList.push(order);
         }
